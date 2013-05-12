6761893ae32351006fd4cc11524a0b922cf94121
Dag Ole Kristoffersen <thedole@gmail.com>
2013-05-12 19:44:36 +0200

    added a cuboid, and vector constructor

--- script/play.js
+++ script/play.js
@@ -1,37 +1,65 @@
-var Vertex = function(x, y, z) {
+var Vector = function(x, y, z) {
 	this.x = x;
 	this.y = y;
 	this.z = z;
 
-	this.draw = function(imageData, color){
-		imageData.data[(x*4) + (y * (imageData.width * 4))] = color[0];
-		imageData.data[(x*4) + (y * (imageData.width * 4)) + 1] = color[1];
-		imageData.data[(x*4) + (y * (imageData.width * 4)) + 2] = color[2];
-		imageData.data[(x*4) + (y * (imageData.width * 4)) + 3] = color[3];
+	this.magnitude = function(){
+		return Math.sqrt((this.x * this.x) + (this.y * this.y) + (this.z * this.z));
 	};
+};
+
+var Vertex = function(center, offset) {
+	this.center = center;
+	this.offset = offset;
+
+	this.draw = function(imageData, color){
+		var xi = (this.center.x + this.offset.x)*4;
+		var yi = (this.center.y + this.offset.y) * (imageData.width * 4);
+		
+		imageData.data[xi + yi] = color[0];
+		imageData.data[xi + yi + 1] = color[1];
+		imageData.data[xi + yi + 2] = color[2];
+		imageData.data[xi + yi + 3] = color[3];
+	};
+};
+
+
+
+var Cuboid = function(center, size){
+	this.center = center;
+	this.size = size;
+	this.halfWidth = this.size.x/2;
+	this.halfHeight = this.size.y/2;
+	this.halfDepth = this.size.z/2;
+	this.vertices = [];
+
+	this.draw = function (imageData, color) {
+		for (var i = this.vertices.length - 1; i >= 0; i--) {
+			this.vertices[i].draw(imageData, color);
+		}
+	};
+
+	this.initializeVertices = function(){
+		this.vertices.push(new Vertex(center, new Vector(-this.halfWidth, -this.halfHeight, -this.halfDepth)));
+		this.vertices.push(new Vertex(center, new Vector(-this.halfWidth, -this.halfHeight, +this.halfDepth)));
+		this.vertices.push(new Vertex(center, new Vector(-this.halfWidth, this.halfHeight, -this.halfDepth)));
+		this.vertices.push(new Vertex(center, new Vector(-this.halfWidth, this.halfHeight, this.halfDepth)));
+
+		this.vertices.push(new Vertex(center, new Vector(this.halfWidth, -this.halfHeight, -this.halfDepth)));
+		this.vertices.push(new Vertex(center, new Vector(this.halfWidth, -this.halfHeight, this.halfDepth)));
+		this.vertices.push(new Vertex(center, new Vector(this.halfWidth, this.halfHeight, -this.halfDepth)));
+		this.vertices.push(new Vertex(center, new Vector(this.halfWidth, this.halfHeight, this.halfDepth)));
+	};
+
+	this.initializeVertices();
 };
 
 var canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d');
-
-context.font = '38pt Arial';
-context.fillStyle = 'cornflowerblue';
-context.strokeStyle = 'blue';
-
-context.fillText("Hello Canvas", canvas.width/2 - 150,
-                                 canvas.height/2 + 15);
-
-context.strokeText("Hello Canvas", canvas.width/2 - 150,
-                                   canvas.height/2 + 15);
 var imageData = context.getImageData(0, 0, context.canvas.width, context.canvas.height);
 
-var pixels = [];
-for (var i = 99; i; i--) {
-	pixels.push(new Vertex(i, i));
-}
+var cuboid = new Cuboid(new Vector(imageData.width/2, imageData.height/2, imageData.height/2), 
+						new Vector(300, 300, 300));
 
-for (var i = pixels.length - 1; i; i--) {
-	pixels[i].draw(imageData, [200,200,200,255]);
-}
-
+cuboid.draw(imageData, [200,200,200,255]);
 context.putImageData(imageData, 0, 0);
