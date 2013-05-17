11d9188ae514fb2178134f6237934d0979f1b30e
Dag Ole Kristoffersen <thedole@gmail.com>
2013-05-17 21:52:36 +0200

    spora av litt med farger + litt optimalisering

--- script/play.js
+++ script/play.js
@@ -8,6 +8,23 @@ var Matrix = function(data){
 	this.columns = function(){
 		return data.length >= 1 ? data[0].length : 0;
 	};
+
+	this.multiply = function(m2){
+		var result = [[0,0,0],[0,0,0],[0,0,0]];
+		result[0][0] += this.data[0][0]*m2.data[0][0] + this.data[0][1]*m2.data[1][0] + this.data[0][2]*m2.data[2][0];
+		result[0][1] += this.data[0][0]*m2.data[0][1] + this.data[0][1]*m2.data[1][1] + this.data[0][2]*m2.data[2][1];
+		result[0][2] += this.data[0][0]*m2.data[0][2] + this.data[0][1]*m2.data[1][2] + this.data[0][2]*m2.data[2][2];
+
+		result[1][0] += this.data[1][0]*m2.data[0][0] + this.data[1][1]*m2.data[1][0] + this.data[1][2]*m2.data[2][0];
+		result[1][1] += this.data[1][0]*m2.data[0][1] + this.data[1][1]*m2.data[1][1] + this.data[1][2]*m2.data[2][1];
+		result[1][2] += this.data[1][0]*m2.data[0][2] + this.data[1][1]*m2.data[1][2] + this.data[1][2]*m2.data[2][2];
+
+		result[2][0] += this.data[2][0]*m2.data[0][0] + this.data[2][1]*m2.data[1][0] + this.data[2][2]*m2.data[2][0];
+		result[2][1] += this.data[2][0]*m2.data[0][1] + this.data[2][1]*m2.data[1][1] + this.data[2][2]*m2.data[2][1];
+		result[2][2] += this.data[2][0]*m2.data[0][2] + this.data[2][1]*m2.data[1][2] + this.data[2][2]*m2.data[2][2];
+
+		return new Matrix(result);
+	};
 };
 
 var Vector3D = function(x, y, z) {
@@ -15,18 +32,24 @@ var Vector3D = function(x, y, z) {
 	this.y = y;
 	this.z = z;
 
-	this.magnitude = function(){
-		return Math.sqrt((this.x * this.x) + (this.y * this.y) + (this.z * this.z));
-	};
+	this.magnitude = Math.sqrt((this.x * this.x) + (this.y * this.y) + (this.z * this.z));
 
 	this.transform = function(matrix){
-		var n = 3;
-		var result = [0,0,0];
-		for (var row = 0; row < n; row++) {
-				result[row] += matrix.data[row][0] * this.x;
-				result[row] += matrix.data[row][1] * this.y;
-				result[row] += matrix.data[row][2] * this.z;
-		};
+		var result = [0,0,0],
+		data = matrix.data;
+		
+		result[0] += data[0][0] * this.x;
+		result[0] += data[0][1] * this.y;
+		result[0] += data[0][2] * this.z;
+
+		result[1] += data[1][0] * this.x;
+		result[1] += data[1][1] * this.y;
+		result[1] += data[1][2] * this.z;
+
+		result[2] += data[2][0] * this.x;
+		result[2] += data[2][1] * this.y;
+		result[2] += data[2][2] * this.z;
+
 
 		this.x = result[0];
 		this.y = result[1];
@@ -39,37 +62,53 @@ var Vertex = function(center, offset) {
 	this.offset = offset;
 
 	this.draw = function(imageData, color, viewPos){
-		var perspectiveFactor = (this.posZ - viewPos.z);
-		var screenX = Math.floor((((this.posX - viewPos.x)/perspectiveFactor) * -viewPos.z) + viewPos.x);
-		if (0 <= screenX && screenX < imageData.width){
-			var screenY = Math.floor(((this.posY - viewPos.y)/perspectiveFactor) * -viewPos.z + viewPos.y);
-		
-			if (0 <= screenY && screenY < imageData.height) {
-				var alpha = Math.floor(color[3]/(1 + offset.magnitude()/512));
-				var xi = screenX * 4;
-				var yi = screenY * (imageData.width * 4);
-				
-				
-				imageData.data[xi + yi] = color[0];
-				imageData.data[xi + yi + 1] = color[1];
-				imageData.data[xi + yi + 2] = color[2];
-				imageData.data[xi + yi + 3] = alpha;
-			}
+		if(this.posZ < 0){
+			return;
 		}
+
+		var width = imageData.width,
+		data = imageData.data;
+		var screenX = this.calcScreenCoord(this.posX, viewPos.x);
+		if (0 >= screenX || screenX > width){
+			return;
+		}
+			
+			var screenY = this.calcScreenCoord(this.posY, viewPos.y);
+			if (0 >= screenY || screenY > imageData.height){
+				return;
+			}
+
+			this.r = color[0];
+			this.g = color[1];
+			this.b = color[2];
+			this.alpha = this.alpha || Math.floor(color[3]/(1 + this.magnitude>>7));
+
+			var xi = screenX * 4;
+			var yi = screenY * (width * 4);
+
+			data[xi + yi] = this.r;
+			data[xi + yi + 1] = this.g;
+			data[xi + yi + 2] = this.b;
+			data[xi + yi + 3] = this.alpha;
+	};
+
+	this.calcScreenCoord = function(pos, viewpos){
+		return Math.floor(((pos - viewpos)/(this.posZ - viewPos.z)) * -viewPos.z + viewpos);
 	};
 
 	this.transform = function(matrix){
 		offset.transform(matrix);
-		this.setPos();
-	};
-
-	this.setPos = function(){
 		this.posX = this.center.x + this.offset.x;
 		this.posY = this.center.y + this.offset.y;
 		this.posZ = this.center.z + this.offset.z;
+		this.magnitude = offset.magnitude;
+		return this;
 	};
 
-	this.setPos();
+	this.posX = this.center.x + this.offset.x;
+	this.posY = this.center.y + this.offset.y;
+	this.posZ = this.center.z + this.offset.z;
+	this.magnitude = offset.magnitude;
 };
 
 var Cuboid = function(center, size){
@@ -102,6 +141,7 @@ var Cuboid = function(center, size){
 		this.vertices.forEach(function(vertex){
 			vertex.transform(matrix);
 		});
+		return this;
 	};
 
 	this.initializeVertices();
@@ -116,8 +156,8 @@ var PointCloud = function(n, center, size){
 	this.vertices = [];
 
 	for (var i = n; i; i--) {
-		this.vertices.push(new Vertex(center, 
-							new Vector3D(	Math.random()*this.size.x - this.halfWidth,
+		this.vertices.push(new Vertex(center,
+											new Vector3D(	Math.random()*this.size.x - this.halfWidth,
 											Math.random()*this.size.y - this.halfHeight,
 											Math.random()*this.size.z - this.halfDepth)));
 	}
@@ -132,41 +172,65 @@ var PointCloud = function(n, center, size){
 		this.vertices.forEach(function(point){
 			point.transform(matrix);
 		});
+		return this;
 	};
 };
 
 var canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d'),
-	
-	baseImageData = context.getImageData(0, 0, context.canvas.width, context.canvas.height),
+
+    baseImageData = context.getImageData(0, 0, context.canvas.width, context.canvas.height),
 	imageData = baseImageData,
 	blackScreen = [],
-	
-	pointCloud = new PointCloud(3200,	new Vector3D(imageData.width /2, imageData.height/2, 2000), 
-										new Vector3D(3200,3200,3200)),
+
+	tmpsina, tmpsinc, tmpsine,
 
 
-	angleZ = 3.14/270,
-	angleY = 3.14/400,
+	angleX = Math.PI/128
+	angleY = Math.PI/512,
+	angleZ = Math.PI/256,
 
-	z = new Matrix([[Math.cos(angleZ),-Math.sin(angleZ),0],
-					[Math.sin(angleZ),Math.cos(angleZ),0],
-					[0,0,1]]),
+	x = new Matrix([[1,0,0],
+					[0, Math.cos(angleX),-Math.sin(angleX)],
+					[0, Math.sin(angleX),Math.cos(angleX),0]]),
 	y = new Matrix([[Math.cos(angleY),0,Math.sin(angleY)],
 					[0,1,0],
 					[-Math.sin(angleY),0,Math.cos(angleY)]]),
+	z = new Matrix([[Math.cos(angleZ),-Math.sin(angleZ),0],
+					[Math.sin(angleZ),Math.cos(angleZ),0],
+					[0,0,1]]),
+	rotationMatrix = x.multiply(y).multiply(z),
+	sina = 1, cosa=0, sinb = Math.sin(Math.PI/67), cosb=Math.cos(Math.PI/67);
+	sinc = 1, cosc=0, sind = Math.sin(Math.PI/41), cosd=Math.cos(Math.PI/41);
+	sine = 1, cose=0, sinf = Math.sin(Math.PI/7), cosf=Math.cos(Math.PI/7),
 
-	viewPos = new Vector3D(imageData.width/2, imageData.height/2, -300);
+	pointCloud = new PointCloud(32000,	new Vector3D(imageData.width /2, imageData.height/2, 1024),
+										new Vector3D(1024,1024,1024)),
 
-	
+	viewPos = new Vector3D(imageData.width/2, imageData.height/2, -1024);
+
 
 function draw() {
 	context.clearRect(0, 0, imageData.width, imageData.height);
 	imageData = context.getImageData(0, 0, imageData.width, imageData.height);
-	pointCloud.draw(imageData, [255,255,255,255], viewPos);
+	pointCloud.draw(imageData, [(127*(1+sina))|0,(127*(1+sinc))|0,(127*(1+sine))|0,255], viewPos);
 	context.putImageData(imageData, 0, 0);
-	pointCloud.transform(z);
-	pointCloud.transform(y);
+	pointCloud.transform(rotationMatrix);
+
+	tmpsina = sina*cosb+cosa*sinb;
+	cosa = cosa*cosb - sina*sinb;
+	sina = tmpsina;
+	
+	tmpsinc = sinc*cosd+cosc*sind;
+	cosc = cosc*cosd - sinc*sind;
+	sinc = tmpsinc;
+
+	tmpsine = sine*cosf+cose*sinf;
+	cose = cose*cosf - sine*sinf;
+	sine = tmpsine;
+
+	//console.log(sina);
+	setTimeout(draw, 0);
 }
 
-setInterval(draw, 30);
+draw();
