16206c5a395fbaa95fc11cbf84b3110149eaaef3
Dag Ole Kristoffersen <thedole@gmail.com>
2013-05-20 15:15:00 +0200

    Now reads obj models and plots the vertices, rendering next, and possibly swapping around the y-axis..

--- script/play.js
+++ script/play.js
@@ -31,7 +31,7 @@ var Vector3D = function(x, y, z) {
 	this.transform = function(matrix){
 		var result = [0,0,0],
 		data = matrix.data;
-		
+
 		result[0] += data[0][0] * this.x;
 		result[0] += data[0][1] * this.y;
 		result[0] += data[0][2] * this.z;
@@ -79,146 +79,50 @@ var Vector3D = function(x, y, z) {
 	this.magnitude = this.calcMagnitude();
 };
 
-var Vertex = function(center, offset) {
-	this.center = center;
-	this.offset = offset;
-
-	this.draw = function(imageData, color, viewPos){
-		if(this.posZ < 0){
-			return;
-		}
-
-		var width = imageData.width,
-		data = imageData.data;
-		var screenX = this.calcScreenCoord(this.posX, viewPos.x);
-		if (0 >= screenX || screenX > width){
-			return;
-		}
-		var screenY = this.calcScreenCoord(this.posY, viewPos.y);
-		if (0 >= screenY || screenY > imageData.height){
-			return;
-		}
-
-		this.r = color[0];
-		this.g = color[1];
-		this.b = color[2];
-		this.alpha = color[3];
-
-		var xi = screenX * 4;
-		var yi = screenY * (width * 4);
-
-		data[xi + yi] = this.r;
-		data[xi + yi + 1] = this.g;
-		data[xi + yi + 2] = this.b;
-		data[xi + yi + 3] = this.alpha;
-	};
-
-	this.calcScreenCoord = function(pos, viewpos){
-		return Math.floor(((pos - viewpos)/(this.posZ - viewPos.z)) * -viewPos.z + viewpos);
-	};
-
-	this.transform = function(matrix){
-		offset.transform(matrix);
-		this.posX = this.center.x + this.offset.x;
-		this.posY = this.center.y + this.offset.y;
-		this.posZ = this.center.z + this.offset.z;
-		this.magnitude = offset.magnitude;
-		return this;
-	};
-
-	this.posX = this.center.x + this.offset.x;
-	this.posY = this.center.y + this.offset.y;
-	this.posZ = this.center.z + this.offset.z;
-	this.magnitude = offset.magnitude;
-};
-var PointCloud = function(n, center, size){
-	this.center = center;
-	this.size = size;
-	this.halfWidth = this.size.x/2;
-	this.halfHeight = this.size.y/2;
-	this.halfDepth = this.size.z/2;
-	this.vertices = [];
-	this.sina = 1;
-	this.cosa=0;
-	this.sinb = Math.sin(Math.PI/4);
-	this.cosb=Math.cos(Math.PI/4);
-
-	for (var i = n; i; i--) {
-		this.vertices.push(new Vertex(center,new Vector3D().normalize().scale(Math.random()*this.halfWidth)
-			));
-	}
-
-	this.draw = function(imageData, color, viewPos){
-		this.vertices.forEach(function(vertex){
-			vertex.draw(imageData, color, viewPos);
-		});
-	};
-
-	this.transform = function(matrix){
-		this.vertices.forEach(function(point){
-			point.transform(matrix);
-		});
-		return this;
-	};
-};
-
-var Model3D = function(position, model){
-	this.draw = function(imageData, color, viewPos){
-		model.vertices.forEach(function(vertex){
-			vertex.draw(imageData, color, viewPos);
-		});
-	};
-
-	this.transform = function(matrix){
-		this.vertices.forEach(function(point){
-			point.transform(matrix);
-		});
-		return this;
-	};
-};
-
 var ModelDescription = function(name){
 	this.name = name,
 	this.vertices = [];
 	this.vertexnormals = [];
 	this.faces = [];
-	this.facenormals;
-	this.material;
+	this.perfacevertexcount = [];
+	this.facenormals = [];
+	this.material = '';
 };
 
 var ObjModelReader = function(file, callback){
 	var that = this,
 	models = [],
+	modeldescriptors = [],
 	mtllib = '',
 	lineHandlers = {
 		'#': function(){return;},
 		'mtllib': function(name){this.mtllib = name;},
-		'o': function(name){models.push(new ModelDescription(name));},
+		'o': function(name){modeldescriptors.push(new ModelDescription(name));},
 		'v': function(x, y, z){
-			if(!models.length){
+			if(!modeldescriptors.length){
 				return;
 			}
-			models[models.length - 1].vertices.push([x, y, z]);
+			modeldescriptors[modeldescriptors.length - 1].vertices.push([x, y, z]);
 		},
 		'usemtl': function(name){
-			if(!models.length){
+			if(!modeldescriptors.length){
 				return;
 			}
-			models[models.length - 1].material = name;
+			modeldescriptors[modeldescriptors.length - 1].material = name;
 		},
 		'f': function(){
-			if(!models.length){
+			if(!modeldescriptors.length){
 				return;
 			}
 			var vertices = [];
-			Array.prototype.forEach.call(arguments, function(v){
-				if(v < 0){
-					v = models[models.length - 1].vertices.length + v;
-				}
-
-				vertices.push(v);
+			Array.prototype.forEach.call(arguments, function(vertexindex){
+				vertexindex = vertexindex < 0 ? 
+					modeldescriptors[modeldescriptors.length - 1].vertices.length + vertexindex
+					: vertexindex - 1;
+				vertices.push(vertexindex);
 			});
-			models[models.length - 1].faces.push(vertices);
+			modeldescriptors[modeldescriptors.length - 1].faces.push(vertices);
+			modeldescriptors[modeldescriptors.length - 1].perfacevertexcount.push(vertices.length);
 		}
 	};
 
@@ -241,13 +145,131 @@ var ObjModelReader = function(file, callback){
 					lineHandlers[keyword].apply(that, parts);
 				}
 				);
-				callback(models);	
+			modeldescriptors.forEach(
+				function(descriptor){
+					var vertexdata = new Float64Array([].concat.apply([], descriptor.vertices));
+					var faces = new Uint16Array([].concat.apply([], descriptor.faces));
+					var vcount = new Uint8Array(descriptor.perfacevertexcount);
+					models.push(new Model3D([0,0,0], vertexdata, faces, vcount, descriptor.name));
+				});
+			callback(models);
 		};
 		r.readAsText(file);
 	} else {
 		alert("Failed to load file");
 	}
 };
+
+var Model3D = function(position, vertices, faces, perfacevertexcount, name){
+	this.name = name;
+	this.position = position;
+	this.vertices = vertices;
+	this.faces = faces;
+	this.perfacevertexcount = perfacevertexcount;
+
+	this.draw = function(imageData, color, viewPos){
+		var vertices = this.vertices,
+		face,
+		faceStartIndex = 0,
+		vertexcount,
+		vertexcountindex = 0,
+		faceEndIndex,
+		faceslength = this.faces.length;
+
+		do{
+			vertexcount = perfacevertexcount[vertexcountindex++];
+			faceEndIndex = faceStartIndex + vertexcount;
+			face = faces.subarray(faceStartIndex, faceEndIndex);
+			faceStartIndex = faceEndIndex;
+			this.drawFace(face, vertexcount, imageData, color, viewPos);
+		} while(faceStartIndex < faceslength);
+	};
+
+	this.drawFace = function(face, count, imageData, color, viewPos){
+		var offset = this.position,
+		width = imageData.width,
+		data = imageData.data,
+		screenX,
+		screenY,
+		x,
+		y,
+		z,
+		xi,
+		yi;
+
+		for (var i = count - 1; i >= 0; i--) {
+			x = offset.x + this.vertices[face[i]*3];
+			y = offset.y + this.vertices[face[i]*3 + 1];
+			z = offset.z + this.vertices[face[i]*3 + 2];
+
+			if(z < 0){
+				return;
+			}
+
+			screenX = this.calcScreenCoord(z, x, viewPos.x);
+			if (0 >= screenX || screenX > width){
+				return;
+			}
+			screenY = this.calcScreenCoord(z, y, viewPos.y);
+			if (0 >= screenY || screenY > imageData.height){
+				return;
+			}
+
+			this.r = color[0];
+			this.g = color[1];
+			this.b = color[2];
+			this.alpha = color[3];
+
+			xi = screenX * 4;
+			yi = screenY * (width * 4);
+
+			data[xi + yi] = this.r;
+			data[xi + yi + 1] = this.g;
+			data[xi + yi + 2] = this.b;
+			data[xi + yi + 3] = this.alpha;
+			//console.log('x: ' + screenX + ' y: ' + screenY);
+		}
+	};
+
+	this.calcScreenCoord = function(z, pos, viewpos){
+		return Math.floor(((pos - viewpos)/(z - viewPos.z)) * -viewPos.z + viewpos);
+	};
+
+	this.transform = function(matrix){
+		var index = 0,
+		endindex,
+		verticeslength = this.vertices.length;
+
+		do{
+			endindex = index + 3;
+			vertex = vertices.subarray(index, endindex);
+			index = endindex;
+			this.transformVertex(vertex, matrix);
+		} while(index < verticeslength);
+	};
+	this.transformVertex = function(vertex, matrix){
+		var result = new Float64Array([0,0,0]);
+		data = matrix.data;
+
+		result[0] += data[0][0] * vertex[0];
+		result[0] += data[0][1] * vertex[1];
+		result[0] += data[0][2] * vertex[2];
+
+		result[1] += data[1][0] * vertex[0];
+		result[1] += data[1][1] * vertex[1];
+		result[1] += data[1][2] * vertex[2];
+
+		result[2] += data[2][0] * vertex[0];
+		result[2] += data[2][1] * vertex[1];
+		result[2] += data[2][2] * vertex[2];
+
+		vertex[0] = result[0];
+		vertex[1] = result[1];
+		vertex[2] = result[2];
+		return vertex;
+	};
+};
+
 
 var canvas = document.getElementById('canvas'),
 context = canvas.getContext('2d'),
@@ -276,55 +298,42 @@ rotationMatrix = x.multiply(y).multiply(z),
 	// sina = 1, cosa=0, sinb = Math.sin(Math.PI/67), cosb=Math.cos(Math.PI/67);
 	// sinc = 1, cosc=0, sind = Math.sin(Math.PI/41), cosd=Math.cos(Math.PI/41);
 	// sine = 1, cose=0, sinf = Math.sin(Math.PI/7), cosf=Math.cos(Math.PI/7),
-
-	// pointCloud = new PointCloud(16000,	new Vector3D(imageData.width /2, imageData.height/2, 600),
-	// 									new Vector3D(1024,1024,1024)),
-modelDescription = {
-	name: 'testCube',
-	vertices: []
-},
-model = new Model3D(),
-viewPos = new Vector3D(imageData.width/2, imageData.height/2, -1024);
+	modelDescription = {
+		name: 'testCube',
+		vertices: []
+	},
+	color = [0,0,0, 255],
+	viewPos = new Vector3D(imageData.width/2, imageData.height/2, -1024);
 
 
-function draw() {
-	context.clearRect(0, 0, imageData.width, imageData.height);
-	imageData = context.getImageData(0, 0, imageData.width, imageData.height);
-	//pointCloud.draw(imageData, [(127*(1+sina))|0,(127*(1+sinc))|0,(127*(1+sine))|0,255], viewPos);
-	//pointCloud.draw(imageData, [255,127,255, 255], viewPos);
-	context.putImageData(imageData, 0, 0);
-	pointCloud.transform(rotationMatrix);
-
-	// tmpsina = sina*cosb+cosa*sinb;
-	// cosa = cosa*cosb - sina*sinb;
-	// sina = tmpsina;
-
-	// tmpsinc = sinc*cosd+cosc*sind;
-	// cosc = cosc*cosd - sinc*sind;
-	// sinc = tmpsinc;
-
-	// tmpsine = sine*cosf+cose*sinf;
-	// cose = cose*cosf - sine*sinf;
-	// sine = tmpsine;
-
-	//console.log(sina);
-	//setTimeout(draw, 0);
-}
-
-var model,
-inputElement = document.getElementById("fileselector");
-inputElement.addEventListener("change", loadmodel, false);
-function loadmodel(event) {
-	var fileList = event.target.files,
-	file,
-	extension,
-	model,
-	reader;
-
-	/* now you can work with the file list */
-	if (!fileList || Object.prototype.toString.call(fileList) !== '[object FileList]'){
-		return;
+	function draw(models) {
+		context.clearRect(0, 0, imageData.width, imageData.height);
+		imageData = context.getImageData(0, 0, imageData.width, imageData.height);
+		models.forEach(
+			function(model){
+				model.position = new Vector3D(400,400,400);
+				model.draw(imageData, color, viewPos);
+				model.transform(rotationMatrix);
+			}
+			);
+		context.putImageData(imageData, 0, 0);
+		setTimeout(function(){draw(models);}, 30);
 	}
+
+	var model,
+	inputElement = document.getElementById("fileselector");
+	inputElement.addEventListener("change", loadmodel, false);
+	function loadmodel(event) {
+		var fileList = event.target.files,
+		file,
+		extension,
+		model,
+		reader;
+
+		/* now you can work with the file list */
+		if (!fileList || Object.prototype.toString.call(fileList) !== '[object FileList]'){
+			return;
+		}
 	// May need to change this later for loading materials as well
 	if(fileList.length !== 1){
 		return;
@@ -339,9 +348,7 @@ function loadmodel(event) {
 	switch(extension){
 		case 'obj':
 		reader = new ObjModelReader(file, function(models){
-			models.forEach(
-				function(model){alert(model.name);}
-				);
+			draw(models);
 		});
 		break;
 	}
