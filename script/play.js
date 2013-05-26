182798a0668fe58a6f6d4aff9459b68ec5ed423e
Dag Ole Kristoffersen <thedole@gmail.com>
2013-05-26 18:33:47 +0200

    Switched to using requestAnimationFrame with a polyfill

--- script/play.js
+++ script/play.js
@@ -1,3 +1,28 @@
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelRequestAnimationFrame = window[vendors[x]+
+          'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
 var Matrix = function(data){
 	this.data = data;
 
@@ -308,6 +333,7 @@ rotationMatrix = x.multiply(y).multiply(z),
 
 
 	function draw(models) {
+		requestAnimationFrame(function(){draw(models);});
 		context.clearRect(0, 0, imageData.width, imageData.height);
 		imageData = context.getImageData(0, 0, imageData.width, imageData.height);
 		models.forEach(
@@ -317,8 +343,7 @@ rotationMatrix = x.multiply(y).multiply(z),
 				model.transform(rotationMatrix);
 			}
 			);
-		context.putImageData(imageData, 0, 0);
-		setTimeout(function(){draw(models);}, 30);
+		context.putImageData(imageData, 0, 0);		
 	}
 
 	var model,
